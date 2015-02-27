46554b0f94d939bf6952fc70ebdf347476572abe
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-02-27 16:41:41 +0100

    generate pot using revolve function

--- Rhino/Pot.py
+++ Rhino/Pot.py
@@ -15,33 +15,20 @@ radiusTop = diameterTop / 2
 radiusMiddle = diameterMiddle / 2
 radiusBottom = diameterBottom / 2
 
-# create outer hull
-outerBottouterMiddleCircleircle = rs.curve.AddCircle([0, 0, 0], radiusBottom)
-outerMiddleCircle = rs.curve.AddCircle([0, 0, middleHeight], radiusMiddle)
-outerTopCircle = rs.curve.AddCircle([0, 0, totalHeight], radiusTop)
-outerHullSurface = rs.surface.AddLoftSrf([outerBottouterMiddleCircleircle, outerMiddleCircle, outerTopCircle])
+# draw hull outline
+bottomOuterLine = rs.curve.AddLine([0, 0, 0], [radiusBottom, 0, 0])
+bottomInnerLine = rs.curve.AddLine([0, 0, thickness], [radiusBottom - thickness, 0, thickness])
+outerHullLine = rs.curve.AddArc3Pt([radiusBottom, 0, 0], [radiusTop, 0, totalHeight], [radiusMiddle, 0, middleHeight])
+innerHullLine = rs.curve.AddArc3Pt([radiusBottom - thickness, 0, thickness], [radiusTop - thickness, 0, totalHeight],
+                                   [radiusMiddle - thickness, 0, middleHeight])
+topEdgeLine = rs.curve.AddLine([radiusTop, 0, totalHeight], [radiusTop - thickness, 0, totalHeight])
+hullLine = rs.curve.JoinCurves([bottomOuterLine, outerHullLine, topEdgeLine, innerHullLine, bottomInnerLine], True)
 
-# create inner hull
-innerBottomCircle = rs.curve.AddCircle([0, 0, thickness], radiusBottom - thickness)
-innerMiddleCircle = rs.curve.AddCircle([0, 0, middleHeight], radiusMiddle - thickness)
-innerTopCircle = rs.curve.AddCircle([0, 0, totalHeight], radiusTop - thickness)
-innerHullSurface = rs.surface.AddLoftSrf([innerBottomCircle, innerMiddleCircle, innerTopCircle])
+# create rail
+railLine = rs.curve.AddCircle([0, 0, 0], radiusBottom)
 
-# create top edge
-topEdgeSurface = rs.surface.AddPlanarSrf([outerTopCircle, innerTopCircle])
+# sweep hull
+hullSurface = rs.surface.AddRevSrf(hullLine, [[0, 0, 0], [0, 0, thickness]])
 
-# create bottom surface
-outerBottomSurface = rs.surface.AddPlanarSrf([outerBottouterMiddleCircleircle])
-innerBottomSurface = rs.surface.AddPlanarSrf([innerBottomCircle])
-
-# cleanup curves
-rs.object.DeleteObjects([outerBottouterMiddleCircleircle, outerMiddleCircle, outerTopCircle])
-rs.object.DeleteObjects([innerBottomCircle, innerMiddleCircle, innerTopCircle])
-
-# join all surfaces
-potSurface = rs.surface.JoinSurfaces([outerHullSurface, innerHullSurface, topEdgeSurface,
-                                      outerBottomSurface, innerBottomSurface])
-
-# cleanup surfaces
-rs.object.DeleteObjects([outerHullSurface, innerHullSurface, topEdgeSurface,
-                         outerBottomSurface, innerBottomSurface])
+# remove remaining curves
+rs.object.DeleteObjects([hullLine, railLine])
