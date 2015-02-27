cce0147b6efd19922f5c9b9520963dbdd54bc737
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-02-27 15:02:46 +0100

    finished a simple pot

--- Rhino/Pot.py
+++ Rhino/Pot.py
@@ -1,19 +1,47 @@
 import rhinoscript as rs
 
-diameterTop = rs.userinterface.GetInteger("Diameter Top", 12)
-diameterBottom = rs.userinterface.GetInteger("Diameter Bottom", 8)
-diameterMiddle = ((diameterTop - diameterBottom) * 0.7) + diameterBottom
+# get all user data
+diameterTop = rs.userinterface.GetInteger("Diameter Top", 180)
+diameterBottom = rs.userinterface.GetInteger("Diameter Bottom", 120)
+totalHeight = rs.userinterface.GetInteger("Height", 170)
+thickness = rs.userinterface.GetInteger("Thickness", 5)
 
-totalHeight = rs.userinterface.GetInteger("Height", 20)
+# calculate stuff from user data
+diameterMiddle = ((diameterTop - diameterBottom) * 0.8) + diameterBottom
 middleHeight = totalHeight * 0.4
 
-plane = rs.plane.WorldXYPlane()
+# radiuses
+radiusTop = diameterTop / 2
+radiusMiddle = diameterMiddle / 2
+radiusBottom = diameterBottom / 2
 
-bottom = rs.curve.AddCircle(plane, diameterBottom)
-middle = rs.curve.AddCircle(plane, diameterMiddle)
-top = rs.curve.AddCircle(plane, diameterTop)
+# create outer hull
+outerBottouterMiddleCircleircle = rs.curve.AddCircle([0, 0, 0], radiusBottom)
+outerMiddleCircle = rs.curve.AddCircle([0, 0, middleHeight], radiusMiddle)
+outerTopCircle = rs.curve.AddCircle([0, 0, totalHeight], radiusTop)
+outerHullSurface = rs.surface.AddLoftSrf([outerBottouterMiddleCircleircle, outerMiddleCircle, outerTopCircle])
 
-rs.object.MoveObject(middle, [0, 0, middleHeight])
-rs.object.MoveObject(top, [0, 0, totalHeight])
+# create inner hull
+innerBottomCircle = rs.curve.AddCircle([0, 0, thickness], radiusBottom - thickness)
+innerMiddleCircle = rs.curve.AddCircle([0, 0, middleHeight], radiusMiddle - thickness)
+innerTopCircle = rs.curve.AddCircle([0, 0, totalHeight], radiusTop - thickness)
+innerHullSurface = rs.surface.AddLoftSrf([innerBottomCircle, innerMiddleCircle, innerTopCircle])
 
-rs.surface.AddLoftSrf([bottom, middle, top])
+# create top edge
+topEdgeSurface = rs.surface.AddPlanarSrf([outerTopCircle, innerTopCircle])
+
+# create bottom surface
+outerBottomSurface = rs.surface.AddPlanarSrf([outerBottouterMiddleCircleircle])
+innerBottomSurface = rs.surface.AddPlanarSrf([innerBottomCircle])
+
+# cleanup curves
+rs.object.DeleteObjects([outerBottouterMiddleCircleircle, outerMiddleCircle, outerTopCircle])
+rs.object.DeleteObjects([innerBottomCircle, innerMiddleCircle, innerTopCircle])
+
+# join all surfaces
+potSurface = rs.surface.JoinSurfaces([outerHullSurface, innerHullSurface, topEdgeSurface,
+                                      outerBottomSurface, innerBottomSurface])
+
+# cleanup surfaces
+rs.object.DeleteObjects([outerHullSurface, innerHullSurface, topEdgeSurface,
+                         outerBottomSurface, innerBottomSurface])
